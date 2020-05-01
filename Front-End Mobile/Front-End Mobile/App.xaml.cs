c2e500ce9e56637d2da8ff20467360a1abbdfe61
Nello Carotenuto <nello.carotenuto.96@live.com>
2020-05-01 16:40:05 +0200

    Add project files

---
+++ Front-End Mobile/Front-End Mobile/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Front_End_Mobile.Services;
+using Front_End_Mobile.Views;
+
+namespace Front_End_Mobile
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Front-End Mobile/Front-End Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Front_End_Mobile.Models;
+
+namespace Front_End_Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
