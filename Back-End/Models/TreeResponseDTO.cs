b6ec92b4099cd2a4a24d6fa880c7164b4aeeedc9
Nello Carotenuto <nello.carotenuto.96@live.com>
2020-05-16 01:18:55 +0200

    Back-End: define DTO models for users and trees

---
+++ Back-End/Models/TreeResponseDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Back_End.Models
+{
+    public class TreeResponseDTO
+    {
+        public long Id { get; set; }
+
+        public string Photo { get; set; }
+        
+        public string Description { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public UserResponseDTO User { get; set; }
+
+    }
+}
---
+++ Back-End/Models/UserResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Back_End.Models
+{
+    public class UserResponseDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Picture { get; set; }
+    }
+}
