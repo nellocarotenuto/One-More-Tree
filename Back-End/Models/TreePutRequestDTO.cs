18ea911b2993ab6485beba246d1d298a10934025
Nello Carotenuto <nello.carotenuto.96@live.com>
2020-05-16 01:19:47 +0200

    Back-End: define PUT request DTO model for trees

---
+++ Back-End/Models/TreePutRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Back_End.Validators;
+
+namespace Back_End.Models
+{
+    public class TreePutRequestDTO
+    {
+        public long Id { get; set; }
+
+        [StringLength(1024, ErrorMessage = "The description is too long.")]
+        [TreeDescription]
+        public string Description { get; set; }
+    }
+}
