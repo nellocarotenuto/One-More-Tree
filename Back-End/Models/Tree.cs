d7f8be5dd1aa1485580e0f1251081c115c922371
Nello Carotenuto <nello.carotenuto.96@live.com>
2020-05-16 01:14:44 +0200

    Back-End: update Tree model

--- Back-End/Models/Tree.cs
+++ Back-End/Models/Tree.cs
@@ -1,16 +1,31 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Back_End.Models
 {
     public class Tree
     {
-        public long ID { get; set; }
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
         public string Photo { get; set; }
+
         public string Description { get; set; }
+
+        [Required]
         public double Latitude { get; set; }
+
+        [Required]
         public double Longitude { get; set; }
+
+        [Required]
         public string City { get; set; }
+
+        [Required]
         public string State { get; set; }
+
+        [Required]
         public DateTime Date { get; set; }
 
         public User User { get; set; }
