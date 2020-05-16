04e1291625cc4cd04aa87be3fcdcf20073a159e3
Nello Carotenuto <nello.carotenuto.96@live.com>
2020-05-16 02:48:27 +0200

    Back-End: protect endpoints and finish implementing their logic

--- Back-End/Controllers/TreesController.cs
+++ Back-End/Controllers/TreesController.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
 using Back_End.Models;
-using Azure.Storage.Blobs;
-using System.Globalization;
-using Azure.Storage.Blobs.Models;
 
 namespace Back_End.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class TreesController : ControllerBase
     {
         private readonly DatabaseContext _databaseContext;
@@ -27,36 +33,54 @@ namespace Back_End.Controllers
 
         // GET: api/Trees
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Tree>>> GetTrees()
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<TreeResponseDTO>>> GetTrees()
         {
-            return await _databaseContext.Trees.ToListAsync();
+            List<TreeResponseDTO> trees = new List<TreeResponseDTO>();
+
+            foreach (Tree tree in _databaseContext.Trees)
+            {
+                trees.Add(TreeToDTO(tree));
+            }
+
+            return trees.ToList();
         }
 
         // GET: api/Trees/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Tree>> GetTree(long id)
+        [AllowAnonymous]
+        public async Task<ActionResult<TreeResponseDTO>> GetTree(long id)
         {
-            var tree = await _databaseContext.Trees.FindAsync(id);
+            Tree tree = await _databaseContext.Trees.FindAsync(id);
 
             if (tree == null)
             {
                 return NotFound();
             }
 
-            return tree;
+            return TreeToDTO(tree);
         }
 
         // PUT: api/Trees/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutTree(long id, Tree tree)
+        public async Task<IActionResult> PutTree(long id, TreePutRequestDTO request)
         {
-            if (id != tree.ID)
+            long userId = long.Parse(User.Claims.Where(claim => claim.Type == JwtRegisteredClaimNames.Sub).First().Value);
+            Tree tree = await _databaseContext.Trees.FindAsync(id);
+
+            if (id != request.Id)
             {
                 return BadRequest();
             }
 
+            if (tree.User.Id != userId)
+            {
+                return Unauthorized();
+            }
+
+            tree.Description = request.Description;
             _databaseContext.Entry(tree).State = EntityState.Modified;
 
             try
@@ -81,13 +105,22 @@ namespace Back_End.Controllers
         // POST: api/Trees
         [HttpPost]
         [Consumes("multipart/form-data")]
-        public async Task<ActionResult<Tree>> PostTree([FromForm] TreeApiPostRequest request)
+        public async Task<ActionResult<Tree>> PostTree([FromForm] TreePostRequestDTO request)
         {
+            // Upload the photo to Azure Blobs
             BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient("trees");
             BlobClient blobClient = blobContainerClient.GetBlobClient(string.Format(@"{0}.png", Guid.NewGuid()));
 
             await blobClient.UploadAsync(request.Photo.OpenReadStream(), new BlobHttpHeaders() { ContentType = "image/png" });
 
+            // Retrieve the user from the database
+            long userId = long.Parse(User.Claims.Where(claim => claim.Type == JwtRegisteredClaimNames.Sub).First().Value);
+
+            IEnumerable<User> users = from user in _databaseContext.Users
+                                      where user.Id == userId
+                                      select user;
+
+            // Set tree properties
             Tree tree = new Tree() {
                 Photo = blobClient.Uri.ToString(),
                 Description = request.Description,
@@ -95,23 +128,33 @@ namespace Back_End.Controllers
                 Longitude = Double.Parse(request.Coordinates.Split(",")[1], CultureInfo.InvariantCulture),
                 State = request.State,
                 City = request.City,
-                Date = DateTime.Now
+                Date = DateTime.Now,
+                User = users.First()
             };
-            
+
+            // Save the tree to the database
             _databaseContext.Trees.Add(tree);
             await _databaseContext.SaveChangesAsync();
 
-            return CreatedAtAction("GetTree", new { id = tree.ID }, tree);
+            return CreatedAtAction("GetTree", new { id = tree.Id }, tree);
         }
 
         // DELETE: api/Trees/5
         [HttpDelete("{id}")]
         public async Task<ActionResult<Tree>> DeleteTree(long id)
         {
-            var tree = await _databaseContext.Trees.FindAsync(id);
+            long userId = long.Parse(User.Claims.Where(claim => claim.Type == JwtRegisteredClaimNames.Sub).First().Value);
+            
+            Tree tree = await _databaseContext.Trees.FindAsync(id);
+            
             if (tree == null)
             {
                 return NotFound();
+            }
+
+            if (tree.User.Id != userId)
+            {
+                return Unauthorized();
             }
 
             _databaseContext.Trees.Remove(tree);
@@ -120,9 +163,55 @@ namespace Back_End.Controllers
             return tree;
         }
 
+        [HttpGet("/api/users/{id}/trees")]
+        [AllowAnonymous]
+        public async Task<ActionResult<TreeResponseDTO>> GetUserTrees(long id)
+        {
+            User user = await _databaseContext.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            List<TreeResponseDTO> trees = new List<TreeResponseDTO>();
+
+            foreach (Tree tree in _databaseContext.Trees.Where(tree => tree.User.Id == id))
+            {
+                trees.Add(TreeToDTO(tree));
+            }
+
+            return Ok(trees);
+        }
+
         private bool TreeExists(long id)
         {
-            return _databaseContext.Trees.Any(e => e.ID == id);
+            return _databaseContext.Trees.Any(e => e.Id == id);
+        }
+        private bool UserExists(long id)
+        {
+            return _databaseContext.Users.Any(e => e.Id == id);
+        }
+
+        private TreeResponseDTO TreeToDTO(Tree tree)
+        {
+            return new TreeResponseDTO
+            {
+                Id = tree.Id,
+                Description = tree.Description,
+                Photo = tree.Photo,
+                Latitude = tree.Latitude,
+                Longitude = tree.Longitude,
+                City = tree.City,
+                State = tree.State,
+                Date = tree.Date,
+                User = new UserResponseDTO
+                {
+                    Id = tree.User.Id,
+                    Name = tree.User.Name,
+                    Picture = tree.User.Picture
+                }
+            };
         }
     }
 }
