f371775e76d4d877f8f96fd8ffe582eee43cb062
Nello Carotenuto <nello.carotenuto.96@live.com>
2020-05-17 20:54:29 +0200

    Back-End: expose changes tracking through web API

---
+++ Back-End/Controllers/SyncController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+using Dapper;
+
+namespace Back_End.Controllers
+{
+    [Route("api/trees/sync")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class SyncController : ControllerBase
+    {
+        private static readonly string procedure = "OneMoreTreeContext.dbo.sync_trees";
+
+        private readonly string _databaseConnectionString;
+
+        public SyncController(IConfiguration configuration)
+        {
+            _databaseConnectionString = configuration.GetConnectionString("DATABASE_CONNECTION_STRING");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<string>> GetDelta(int fromVersion)
+        {
+            JsonDocument result = null;
+
+            using (SqlConnection connection = new SqlConnection(_databaseConnectionString))
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("json", JsonSerializer.Serialize(new { fromVersion }));
+
+                var query = await connection.ExecuteScalarAsync<string>(
+                    sql: procedure,
+                    param: parameters,
+                    commandType: CommandType.StoredProcedure
+                );
+
+                if (query != null)
+                    result = JsonDocument.Parse(query);
+            }
+
+            if (result == null)
+                result = JsonDocument.Parse("[]");
+
+            return Ok(result.RootElement);
+        }
+    }
+}
