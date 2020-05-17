4be590e8b83cb0b214b110444b8fc9bb29eb89c6
Nello Carotenuto <nello.carotenuto.96@live.com>
2020-05-17 22:37:25 +0200

    Back-End: fix SQL scripts references

--- Back-End/Migrations/20200517141515_TrackChanges.cs
+++ Back-End/Migrations/20200517141515_TrackChanges.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 using System.IO;
 
 namespace Back_End.Migrations
 {
     public partial class TrackChanges : Migration
     {
+        private readonly string _sqlDirectory = $@"{AppDomain.CurrentDomain.BaseDirectory}\Sql";
+        
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(File.ReadAllText(@"Sql\ChangeTrackingEnable.sql"), true);
-            migrationBuilder.Sql(File.ReadAllText(@"Sql\TreeSyncStoreProcedure.sql"));
+            migrationBuilder.Sql(File.ReadAllText($@"{_sqlDirectory}\ChangeTrackingEnable.sql"), true);
+            migrationBuilder.Sql(File.ReadAllText($@"{_sqlDirectory}\TreeSyncStoreProcedure.sql"));
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
