33b096a021270f0c612ced721fc8dbfc93faad2a
Gianluca Caggiano <caggiano.gianluca.96@hotmail.it>
2020-05-23 22:50:42 +0100

    Front-End: define User and Tree models

---
+++ Front-End Mobile/Front-End Mobile/Models/Tree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Front_End_Mobile.Models
+{
+    [Table("Trees")]
+    public class Tree
+    {
+        [PrimaryKey]
+        public long Id { get; set; }
+
+        public string Photo { get; set; }
+
+        [MaxLength(1024)]
+        public string Description { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [ForeignKey(typeof(User))]
+        public long UserId { get; set; }
+
+        [ManyToOne(CascadeOperations = CascadeOperation.CascadeInsert | CascadeOperation.CascadeRead)]
+        public User User { get; set; }
+    }
+}
---
+++ Front-End Mobile/Front-End Mobile/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Front_End_Mobile.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [PrimaryKey]
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Picture { get; set; }
+
+        [OneToMany(CascadeOperations = CascadeOperation.All)]
+        public List<Tree> Tree { get; set; }
+    }
+}
