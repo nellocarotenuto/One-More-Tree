bf2ebfc4ff00f033e0285d9b9ec1acfa7a0419b7
Gianluca Caggiano <caggiano.gianluca.96@hotmail.it>
2020-05-28 16:05:18 +0100

    Front-End: Inject custom services into the app

--- Front-End Mobile/Front-End Mobile/App.xaml.cs
+++ Front-End Mobile/Front-End Mobile/App.xaml.cs
@@ -1,20 +1,37 @@
-﻿using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-using Front_End_Mobile.Services;
+﻿using Xamarin.Forms;
 using Front_End_Mobile.Views;
+using Front_End_Mobile.Models;
+using Autofac;
+using Front_End_Mobile.Services;
 
 namespace Front_End_Mobile
 {
     public partial class App : Application
     {
+        // IContainer is provided by Autofac
+        private static IContainer _container;
+
+        // Service instances that can be injected
+        public static DatabaseService Database { get { return _container.Resolve<DatabaseService>(); } }
+        public static AuthService AuthService { get { return _container.Resolve<AuthService>(); } }
+        public static TreeService TreeService { get { return _container.Resolve<TreeService>(); } }
 
         public App()
         {
             InitializeComponent();
+            InitializeServices();
+        }
 
-            DependencyService.Register<MockDataStore>();
-            MainPage = new MainPage();
+        // Inizialize the services of the app
+        private void InitializeServices()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+
+            builder.RegisterType<AuthService>().SingleInstance();
+            builder.RegisterType<DatabaseService>().SingleInstance();
+            builder.RegisterType<TreeService>();
+            
+            _container = builder.Build();
         }
 
         protected override void OnStart()
