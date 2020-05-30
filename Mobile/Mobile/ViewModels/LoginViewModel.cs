ac13c6dc49b87d846ec14f481693d35f3186af4d
Nello Carotenuto <nello.carotenuto.96@live.com>
2020-05-30 20:22:03 +0200

    Front-End: define a ViewModel for login options

---
+++ Mobile/Mobile/ViewModels/LoginViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Mobile.ViewModels
+{
+    class LoginViewModel : BaseViewModel
+    {
+        public bool isAuthenticated;
+
+        public bool IsAuthenticated
+        {
+            get => isAuthenticated;
+            set => SetProperty(ref isAuthenticated, value);
+        }
+
+        public ICommand FacebookCommand { get; }
+
+        public LoginViewModel()
+        {
+            FacebookCommand = new Command(async () => await Authenticate("facebook"));
+        }
+
+        public async Task Authenticate(string provider)
+        {
+            try
+            {
+                await App.AuthService.Authenticate(provider);
+                IsAuthenticated = true;
+            }
+            catch (TaskCanceledException)
+            {
+                await DisplayAlertAsync("Login is required to use the app.");
+            }
+            catch
+            {
+                await DisplayAlertAsync("Something went wrong while communicating with the server.");
+            }
+        }
+    }
+
+}
