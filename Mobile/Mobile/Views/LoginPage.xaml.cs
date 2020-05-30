e6ed7eff4afa3983875eabf8b3100a192d0677ff
Nello Carotenuto <nello.carotenuto.96@live.com>
2020-05-30 20:23:16 +0200

    Front-End: define a page for login options

---
+++ Mobile/Mobile/Views/LoginPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : BasePage
+    {
+        
+        public LoginPage()
+        {
+            InitializeComponent();
+
+            LoginViewModel viewModel = (LoginViewModel) this.BindingContext;
+
+            viewModel.PropertyChanged += (sender, args) => {
+                if (!args.PropertyName.Equals("IsAuthenticated"))
+                {
+                    return;
+                }
+                
+                if (viewModel.IsAuthenticated)
+                {
+                    Navigation.PopModalAsync();
+                }
+            };
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
+
+    }
+}
