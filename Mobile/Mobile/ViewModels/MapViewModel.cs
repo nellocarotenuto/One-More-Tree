9ae76132ab7a6e9be9a78156fec50c240b791935
Gianluca Caggiano <caggiano.gianluca.96@hotmail.it>
2020-06-02 23:41:44 +0100

    Front-End: add a map to display trees

---
+++ Mobile/Mobile/ViewModels/MapViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Mobile.Models;
+using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace Mobile.ViewModels
+{
+    class MapViewModel : BaseViewModel
+    {
+        public MapViewModel()
+        {
+            _trees = new ObservableCollection<Tree>();
+        }
+
+        public override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            _trees.Clear();
+
+            foreach (Tree tree in await App.Database.GetTreesAsync())
+            {
+                _trees.Add(tree);
+            }
+        }
+
+        private readonly ObservableCollection<Tree> _trees;
+
+        public IEnumerable Trees { get => _trees; }
+    }
+}
