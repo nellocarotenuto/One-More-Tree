cf64ce1e6028b595fc426c5e8f7efe88efffcfba
Gianluca Caggiano <caggiano.gianluca.96@hotmail.it>
2020-06-03 00:51:46 +0100

    Front-End: add mock new post page

---
+++ Mobile/Mobile/ViewModels/PostViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Mobile.ViewModels
+{
+    class PostViewModel : BaseViewModel
+    {
+        public PostViewModel(String imagePath)
+        {
+            ImagePath = imagePath;
+        }
+
+        public String ImagePath { get; set; }
+    }
+}
