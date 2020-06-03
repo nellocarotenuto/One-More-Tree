52821e5b0c227125069f8815779f693d8b0bc2a5
Gianluca Caggiano <caggiano.gianluca.96@hotmail.it>
2020-06-03 01:03:56 +0100

    Front-End: enable media picking from gallery and camera

--- Mobile/Mobile/ViewModels/FeedViewModel.cs
+++ Mobile/Mobile/ViewModels/FeedViewModel.cs
@@ -1,11 +1,10 @@
 ﻿using Mobile.Models;
-using System;
-using System.Collections.Generic;
+using Mobile.Views;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 using System.Collections.ObjectModel;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-
 using Xamarin.Forms;
 
 namespace Mobile.ViewModels
@@ -19,8 +18,9 @@ namespace Mobile.ViewModels
             get => isRefreshing;
             set => SetProperty(ref isRefreshing, value);
         }
-
+        
         public ICommand RefreshCommand { get; }
+        public ICommand PostCommand { get; }
 
         public ObservableCollection<Tree> Trees { get; private set; }
 
@@ -30,6 +30,38 @@ namespace Mobile.ViewModels
 
             RefreshCommand = new Command(async () => await RefreshFeed());
             RefreshCommand.Execute(null);
+
+            PostCommand = new Command<string>(async (string mode) =>
+            {
+                if (!App.AuthService.IsAuthenticated())
+                {
+                    // First time so prompt for login
+                    LoginPage loginPage = new LoginPage();
+                    LoginViewModel loginViewModel = new LoginViewModel();
+
+                    loginViewModel.PropertyChanged += async (sender, args) =>
+                    {
+                        if (!args.PropertyName.Equals("IsAuthenticated"))
+                        {
+                            return;
+                        }
+
+                        if (loginViewModel.IsAuthenticated)
+                        {
+                            await Application.Current.MainPage.Navigation.PopAsync();
+
+                            await GetMedia(mode);
+                        }
+                    };
+
+                    loginPage.BindingContext = loginViewModel;
+                    await Application.Current.MainPage.Navigation.PushAsync(loginPage);
+                }
+                else
+                {
+                    await GetMedia(mode);
+                }
+            });
         }
 
         public async Task RefreshFeed()
@@ -56,5 +88,49 @@ namespace Mobile.ViewModels
             }
         }
 
+        private async Task GetMedia(string mode)
+        {
+            MediaFile photo = null;
+
+            if ("Camera".Equals(mode))
+            {
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await DisplayAlertAsync("No Camera");
+                    return;
+                }
+
+                photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    CompressionQuality = 92,
+                    DefaultCamera = CameraDevice.Front
+                });
+            }
+            else if ("Gallery".Equals(mode))
+            {
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsPickPhotoSupported)
+                {
+                    await DisplayAlertAsync("Photos Not Supported");
+                    return;
+                }
+
+                photo = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions
+                {
+                    CompressionQuality = 92,
+                });
+            }
+                
+            if (photo == null)
+            {
+                return;
+            }   
+
+            PostPage postPage = new PostPage();
+            postPage.BindingContext = new PostViewModel(photo.Path);
+
+            await App.Current.MainPage.Navigation.PushAsync(postPage);
+        }
     }
 }
